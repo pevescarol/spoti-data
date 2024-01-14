c295bf35327e5f4a1f2505a0808229b902fef129
pevescarol <carol.peves@gmail.com>
2024-01-14 13:57:58 -0300

    carga de las claves

---
+++ config/config.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID")
+CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET")
+REDIRECT_URI = os.getenv("SPOTIFY_REDIRECT_URI")
+DATABASE_LOCATION = os.getenv("DATABASE_LOCATION")
